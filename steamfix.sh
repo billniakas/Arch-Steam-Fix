d12fc9f0b3bad86be891d61643f4bf5fd5876cab
billniakas <billniakas@gmail.com>
2017-10-05 23:22:20 +0300

    updated bash script

--- steamfix.sh
+++ steamfix.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-
-echo "Fixing Steam...."
-
-find ~/.local/share/Steam/ \( -name "libgcc_s.so*" -o -name "libstdc++.so*" -o -name "libxcb.so*" -o -name "libgpg-error.so*" \) -print -delete
-
-echo "Done!!!"
+sleep 1
+if [ ! -d "~/.local/share/Steam" ];
+then
+	echo "No Steam Folder Present"
+else
+	echo "Fixing Steam..."
+	sleep 1	
+	find ~/.local/share/Steam/ \( -name "libgcc_s.so*" -o -name "libstdc++.so*" -o -name "libxcb.so*" -o -name "libgpg-error.so*" \) -print -delete
+	echo "Done!!!"
+fi
+sleep 1
